05eb66b9a35751669e1eda61cf24db4f1debf692
Doug McInnes <doug@dougmcinnes.com>
2014-04-29 18:10:27 -0700

    need to clean up the fakeweb registrations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,4 +16,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # clean up after every test
+  config.after(:each) do
+    FakeWeb.clean_registry
+  end
 end
