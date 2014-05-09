9189c017ec810e6077c1ee4c76fb8092c1cbfb62
Doug McInnes <doug@dougmcinnes.com>
2014-05-09 13:37:21 -0700

    bundler did not like the 'pre' part of the verison

--- lib/daylight/version.rb
+++ lib/daylight/version.rb
@@ -3,7 +3,7 @@ module Daylight
     MAJOR = 0
     MINOR = 0
     PATCH = 0
-    BUILD = 'pre1'
+    BUILD = 1
 
     Daylight::VERSION = STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
