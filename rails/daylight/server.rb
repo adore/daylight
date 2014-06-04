aa445b484f83c4d9ad4ae5e1ecf9c65eadd4bab9
Reid MacDonald <reidmix@gmail.com>
2014-06-03 18:18:51 -0700

    comments about Daylight::Server

--- rails/daylight/server.rb
+++ rails/daylight/server.rb
@@ -1,7 +1,9 @@
+# Modules requireed to execute Daylight::Server queries
 require 'daylight/helpers'
 require 'daylight/params'
 require 'daylight/refiners'
 
+# Extensions, fixes and patches for Rails to needed for Daylight::Server
 require 'extensions/array_ext'
 require 'extensions/autosave_association_fix'
 require 'extensions/has_one_serializer_ext'
