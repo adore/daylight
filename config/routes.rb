e3e75a40e5aa21b379a170033750300a7eec77fe
Doug McInnes <doug@dougmcinnes.com>
2014-06-03 17:56:10 -0700

    fix named routes in doc links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Daylight::Documentation.routes.draw do
 
   with_options controller: 'documentation' do |c|
-    c.get '/',          action: :index
+    c.get '/',          action: :index, as: 'index'
     c.get 'api',        action: :model_index
     c.get 'api/:model', action: :model, as: 'model'
   end
