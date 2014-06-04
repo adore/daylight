d43dd035f027507ec94a1823dae8eb80d68c7420
Reid MacDonald <reidmix@gmail.com>
2014-06-04 09:47:54 -0700

    using autoloader to load daylight library

--- lib/daylight.rb
+++ lib/daylight.rb
@@ -1,19 +1,25 @@
-##
-# Include into API client to enable Daylight::API based queries
-
 $: << File.expand_path('../../rails', __FILE__)
 
 require 'active_support/core_ext'
 require 'active_resource'
 
-require 'daylight/version'
-require 'daylight/errors'
-require 'daylight/associations'
-require 'daylight/collection'
-require 'daylight/inflections'
-require 'daylight/refinements'
-require 'daylight/resource_proxy'
-require 'daylight/api'
+##
+# Include into API client to enable Daylight::API based queries
 
 module Daylight
+  extend ActiveSupport::Autoload
+
+  eager_autoload do
+    autoload :Version
+    autoload :Associations
+    autoload :Collection
+    autoload :Errors
+    autoload :Inflections
+    autoload :Refinements
+    autoload :ResourceProxy
+    autoload :API
+  end
 end
+
+# run eager load
+Daylight.eager_load!
