95d5e30fa6ca1d417da9aebab0d472b15c4edc0e
Reid MacDonald <reidmix@gmail.com>
2014-06-03 18:14:09 -0700

    removing app from autoload path -- not sure autoload path is needed

--- rails/daylight/documentation.rb
+++ rails/daylight/documentation.rb
@@ -2,7 +2,7 @@ require 'haml'
 
 module Daylight
   class Documentation < ::Rails::Engine
-    config.autoload_paths << File.expand_path("../../app", __FILE__)
+    config.autoload_paths << File.expand_path("../..", __FILE__)
 
     isolate_namespace Daylight
   end
