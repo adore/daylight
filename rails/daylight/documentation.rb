0fef64c0e84e02d44df8556d32989c351f022794
Reid MacDonald <reidmix@gmail.com>
2014-06-03 18:16:10 -0700

    comments about Documentation Engine

--- rails/daylight/documentation.rb
+++ rails/daylight/documentation.rb
@@ -1,5 +1,7 @@
 require 'haml'
 
+##
+# Rails::Engine to add Documentation features to a Daylight::Server
 module Daylight
   class Documentation < ::Rails::Engine
     config.autoload_paths << File.expand_path("../..", __FILE__)
