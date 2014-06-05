586e1e5d073f551362973122811072dfc03a4ae2
Reid MacDonald <reidmix@gmail.com>
2014-06-04 17:22:49 -0700

    handy dandy comments to each extension

--- rails/daylight/server.rb
+++ rails/daylight/server.rb
@@ -1,11 +1,13 @@
 # Rails extensions, patches, fixes needed to execute a Daylight::Server
-require 'extensions/array_ext'
-require 'extensions/autosave_association_fix'
-require 'extensions/has_one_serializer_ext'
-require 'extensions/nested_attributes_ext'
-require 'extensions/read_only_attributes'
-require 'extensions/render_json_meta'
-require 'extensions/route_options'
+# In the future, these could be configurable or contributed back
+
+require 'extensions/array_ext'                  # non-destructive `extract_options`
+require 'extensions/autosave_association_fix'   # fix for autosaving `inverse_of` associations
+require 'extensions/has_one_serializer_ext'     # serializer recognizes belong_to :through association
+require 'extensions/nested_attributes_ext'      # associates two previously existing records
+require 'extensions/read_only_attributes'       # serializer support for `read_only` attributes
+require 'extensions/render_json_meta'           # adds metadata to the json response
+require 'extensions/route_options'              # adds associated, remoted options to routes
 
 ##
 # Include into Rails server to handle Daylight::API queries
