f8fb5b6a6fe7f0bdd7e7f26d6c6a92fce834671e
Doug McInnes <doug@dougmcinnes.com>
2014-06-06 16:37:00 -0700

    don't need to include the helper in our helper specs
    since we're using the dummy application it's recognized as an actual
    helper and can be tested that way.

--- spec/helpers/documentation_helper_spec.rb
+++ spec/helpers/documentation_helper_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe DaylightDocumentation::DocumentationHelper do
-  include DaylightDocumentation::DocumentationHelper
 
   class TestModel < ActiveRecord::Base
     include Daylight::Associations
@@ -31,7 +30,7 @@ describe DaylightDocumentation::DocumentationHelper do
     it "yields the route verb, path, and defaults hash for the model" do
       yielded = false
       verbs = %w[GET POST PUT PATCH DELETE]
-      model_verbs_and_routes TestModel do |verb, path, defaults|
+      helper.model_verbs_and_routes TestModel do |verb, path, defaults|
         verbs.should include(verb)
         path.to_s.should =~ %r{/test_model}
         defaults.should be_a(Hash)
@@ -44,27 +43,27 @@ describe DaylightDocumentation::DocumentationHelper do
 
   describe :model_filters do
     it "returns list of the model's filters including attributes" do
-      model_filters(TestModel).should include('name')
+      helper.model_filters(TestModel).should include('name')
     end
 
     it "returns list of the model's filters including associations" do
-      model_filters(TestModel).should include('users')
+      helper.model_filters(TestModel).should include('users')
     end
   end
 
   describe :action_definition do
     it "returns a definition for an action" do
-      action_definition({action:'index'}, TestModel).should ==
+      helper.action_definition({action:'index'}, TestModel).should ==
         "Retrieves a list of test models"
     end
 
     it "handles associated" do
-      action_definition({action:'associated', associated:'foo_bar'}, TestModel).should ==
+      helper.action_definition({action:'associated', associated:'foo_bar'}, TestModel).should ==
         "Returns test model's foo bars"
     end
 
     it "handles remoted" do
-      action_definition({action:'remoted', remoted:'baz'}, TestModel).should ==
+      helper.action_definition({action:'remoted', remoted:'baz'}, TestModel).should ==
         "Calls test model's remote method baz"
     end
   end
