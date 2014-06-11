9394a49bbbcbf68c6a364790ca2f53de41828798
Reid MacDonald <reidmix@gmail.com>
2014-06-10 17:45:21 -0700

    tests for show, associated, and remoted

--- spec/rails/daylight/api_controller_spec.rb
+++ spec/rails/daylight/api_controller_spec.rb
@@ -12,11 +12,14 @@ describe Daylight::APIController, type: :controller do
     # TODO: remove when incorporated into ActiveRecord::Base
     include Daylight::Refiners
 
-    has_many :cases
+    # weirdness with route_options only adding to anonymous controller,
+    # this wouldn't normally happen as anonymous controllers do not get used.
+    add_remoted :odd_cases
+    has_many    :cases
 
-    def odd_suites
+    def odd_cases
       # computational results
-      all.values_at(*all.each_index.select(&:odd?))
+      cases.select {|c| c.id.odd? }
     end
   end
 
@@ -24,6 +27,9 @@ describe Daylight::APIController, type: :controller do
   end
 
   class Case < ActiveRecord::Base
+    # TODO: remove when incorporated into ActiveRecord::Base
+    include Daylight::Refiners
+
     belongs_to :suite
   end
 
@@ -77,7 +83,7 @@ describe Daylight::APIController, type: :controller do
       get '/anonymous/raise_argument_error'
       get '/anonymous/raise_record_invalid_error'
 
-      resources :anonymous, associated: [:cases], remoted: [:odd_suites]
+      resources :anonymous, associated: [:cases], remoted: [:odd_cases]
     end
   end
 
@@ -88,7 +94,7 @@ describe Daylight::APIController, type: :controller do
         switch false
 
         after(:create) do |suite|
-          create_list :case,  2, suite: suite
+          create_list :case,  3, suite: suite
         end
       end
 
@@ -232,17 +238,21 @@ describe Daylight::APIController, type: :controller do
     let!(:suite2) { create(:suite, switch: false) }
     let!(:suite3) { create(:suite, switch: true)  }
 
-    def parse_response body
-      JSON.parse(body)['anonymous'].
+    def parse_collection body, root='anonymous'
+      JSON.parse(body)[root].
         map(&:values).
         map(&:first).
         map(&:with_indifferent_access)
     end
 
+    def parse_record body
+      JSON.parse(body).with_indifferent_access
+    end
+
     it 'responds to index' do
       get :index
 
-      results = parse_response(response.body)
+      results = parse_collection(response.body)
       results.size.should == 3
 
       names = results.map {|suite| suite["name"] }
@@ -254,7 +264,7 @@ describe Daylight::APIController, type: :controller do
     it 'responds to index with refine_by' do
       get :index, filters: {switch: true}
 
-      results = parse_response(response.body)
+      results = parse_collection(response.body)
 
       results.size.should == 2
       names = results.map {|suite| suite[:name] }
@@ -265,13 +275,67 @@ describe Daylight::APIController, type: :controller do
     it 'creates a record' do
       post :create, suite: suite = FactoryGirl.attributes_for(:suite)
 
-      result = JSON.parse(response.body).with_indifferent_access
+      result = parse_record(response.body)
       result[:name].should   == suite[:name]
       result[:switch].should == suite[:switch]
 
       response.headers['Location'].should == @controller.anonymous_path(Suite.new(result))
     end
 
+    it 'shows a record' do
+      post :show, id: suite2.id
+
+      result = parse_record(response.body)
+      result[:id].should     == suite2[:id]
+      result[:name].should   == suite2[:name]
+      result[:switch].should == suite2[:switch]
+    end
+
+    it 'updates a record' do
+      patch :update, id: suite3.id, suite: {name: 'Rik Mayall'}
+
+      Suite.find(suite3.id).name.should == 'Rik Mayall'
+    end
+
+    it 'deletes a record' do
+      delete :destroy, id: suite1.id
+
+      expect { Suite.find(suite1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'retrieves associated records' do
+      get :associated, id: suite1.id, associated: 'cases'
+
+      results = parse_collection(response.body, 'cases')
+      results.size.should == 3
+
+      test_ids = results.map {|tc| tc['test_id'] }
+      test_ids.should be_include(suite1.cases[0].id)
+      test_ids.should be_include(suite1.cases[1].id)
+      test_ids.should be_include(suite1.cases[2].id)
+    end
+
+    it 'retrieves associated records with refine_by' do
+      get :associated, id: suite1.id, associated: 'cases', limit: 1
+
+      results = parse_collection(response.body, 'cases')
+      results.size.should == 1
+
+      results.first['test_id'].should == suite1.cases.first.id
+    end
+
+    it 'retrieves remoted records' do
+      get :remoted, id: suite1.id, remoted: 'odd_cases'
+
+      results = parse_collection(response.body, 'odd_cases')
+      results.size.should == 2
+
+      odd_case_ids = suite1.odd_cases.map(&:test_id)
+
+      ids = results.map {|suite| suite["test_id"] }
+      ids.should be_include(odd_case_ids.first)
+      ids.should be_include(odd_case_ids.last)
+    end
   end
 
 end
