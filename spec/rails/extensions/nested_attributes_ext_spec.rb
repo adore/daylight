d3a6669e624e94a8bb34690a1619199b424fc31d
Doug McInnes <doug@dougmcinnes.com>
2014-06-13 16:24:10 -0700

    test has_one relationship nested attributes

--- spec/rails/extensions/nested_attributes_ext_spec.rb
+++ spec/rails/extensions/nested_attributes_ext_spec.rb
@@ -8,10 +8,17 @@ class NestedAttributeTest < ActiveRecord::Base
   accepts_nested_attributes_for :collection
 end
 
+class SingleNestedAttributeTest < ActiveRecord::Base
+  has_one :single, class_name: 'AssocNestedAttributeTest', inverse_of: :single_nested_attribute_test
+
+  accepts_nested_attributes_for :single
+end
+
 class AssocNestedAttributeTest < ActiveRecord::Base
   belongs_to :nested_attribute_test, inverse_of: :collection
+  belongs_to :single_nested_attribute_test, inverse_of: :single
 
-  accepts_nested_attributes_for :nested_attribute_test
+  accepts_nested_attributes_for :nested_attribute_test, :single_nested_attribute_test
 end
 
 describe NestedAttributesExt, type: [:model] do
@@ -21,9 +28,14 @@ describe NestedAttributesExt, type: [:model] do
       t.string :name
     end
 
+    create_table :single_nested_attribute_tests do |t|
+      t.string :name
+    end
+
     create_table :assoc_nested_attribute_tests do |t|
       t.string     :name
       t.references :nested_attribute_test
+      t.references :single_nested_attribute_test
     end
   end
 
@@ -33,85 +45,111 @@ describe NestedAttributesExt, type: [:model] do
         name { Faker::Name.name }
       end
 
+      factory :single_nested_attribute_test do
+        name { Faker::Name.name }
+      end
+
       factory :assoc_nested_attribute_test do
         name { Faker::Name.name }
       end
     end
   end
 
-  let(:record) { create(:nested_attribute_test) }
-  let(:member) { create(:assoc_nested_attribute_test) }
+  describe 'has_many' do
+    let(:record) { create(:nested_attribute_test) }
+    let(:member) { create(:assoc_nested_attribute_test) }
 
-  it "ignores nil values" do
-    record.collection_attributes = nil
+    it "ignores nil values" do
+      record.collection_attributes = nil
 
-    lambda { record.save! }.should_not raise_error
-    record.collection.should == []
+      lambda { record.save! }.should_not raise_error
+      record.collection.should == []
+    end
+
+    it "continues to create and associate new records" do
+      record.collection_attributes = [{name: 'foo'}, {name: 'bar'}]
+
+      lambda { record.save! }.should_not raise_error
+
+      names = record.collection.map(&:name)
+      names.should include('foo')
+      names.should include('bar')
+    end
+
+    it "continues to update assoicated records" do
+      record.collection << member
+      record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch'}]
+
+      lambda { record.save! }.should_not raise_error
+      record.collection.size.should == 1
+      record.collection.first.name.should == 'Foozle Cumberbunch'
+    end
+
+    # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
+    it "associates existing records" do
+      record.collection_attributes = [{id: member.id}]
+
+      lambda { record.save! }.should_not raise_error
+      record.collection.size.should == 1
+      record.collection.first.id.should == member.id
+    end
+
+    # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
+    it "keeps association for existing records that are already assoicated" do
+      record.collection << member
+      record.collection_attributes = [{id: member.id}]
+
+      lambda { record.save! }.should_not raise_error
+      record.collection.size.should == 1
+      record.collection.first.id.should == member.id
+    end
+
+    it "updates records that were just associated" do
+      record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch'}]
+
+      lambda { record.save! }.should_not raise_error
+      record.collection.size.should == 1
+      record.collection.first.name.should == 'Foozle Cumberbunch'
+    end
+
+    it "ignores foreign key updates" do
+      different_foreign_key = record.id + 1
+      record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch', nested_attribute_test_id: different_foreign_key}]
+
+      lambda { record.save! }.should_not raise_error
+
+      record.collection.size.should == 1
+      record.collection.first.nested_attribute_test_id.should == record.id
+    end
+
+    # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
+    it "updates previsoulsy associated records" do
+      record.collection << member
+
+      lambda { record.update!(collection_attributes: [{id: member.id, name: 'Foozle Cumberbunch'}]) }.should_not raise_error
+
+      record.collection.size.should == 1
+      record.collection.first.name.should == 'Foozle Cumberbunch'
+    end
   end
 
-  it "continues to create and associate new records" do
-    record.collection_attributes = [{name: 'foo'}, {name: 'bar'}]
+  describe 'has_one' do
+    let(:record) { create(:single_nested_attribute_test) }
+    let(:member) { create(:assoc_nested_attribute_test) }
 
-    lambda { record.save! }.should_not raise_error
+    it "ignores nil values" do
+      record.single = nil
 
-    names = record.collection.map(&:name)
-    names.should include('foo')
-    names.should include('bar')
-  end
+      lambda { record.save! }.should_not raise_error
+      record.single.should be_nil
+    end
 
-  it "continues to update assoicated records" do
-    record.collection << member
-    record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch'}]
+    it "continues to create and associate new records" do
+      record.single_attributes = {name: 'wibble'}
 
-    lambda { record.save! }.should_not raise_error
-    record.collection.size.should == 1
-    record.collection.first.name.should == 'Foozle Cumberbunch'
-  end
+      lambda { record.save! }.should_not raise_error
 
-  # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
-  it "associates existing records" do
-    record.collection_attributes = [{id: member.id}]
-
-    lambda { record.save! }.should_not raise_error
-    record.collection.size.should == 1
-    record.collection.first.id.should == member.id
-  end
-
-  # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
-  it "keeps association for existing records that are already assoicated" do
-    record.collection << member
-    record.collection_attributes = [{id: member.id}]
-
-    lambda { record.save! }.should_not raise_error
-    record.collection.size.should == 1
-    record.collection.first.id.should == member.id
-  end
-
-  it "updates records that were just associated" do
-    record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch'}]
-
-    lambda { record.save! }.should_not raise_error
-    record.collection.size.should == 1
-    record.collection.first.name.should == 'Foozle Cumberbunch'
-  end
-
-  it "ignores foreign key updates" do
-    different_foreign_key = record.id + 1
-    record.collection_attributes = [{id: member.id, name: 'Foozle Cumberbunch', nested_attribute_test_id: different_foreign_key}]
-
-    lambda { record.save! }.should_not raise_error
-
-    record.collection.size.should == 1
-    record.collection.first.nested_attribute_test_id.should == record.id
-  end
-
-  # this also tests cyclic autosave problem that AutosaveAssociataionFix resolves
-  it "updates previsoulsy associated records" do
-    record.collection << member
-
-    lambda { record.update!(collection_attributes: [{id: member.id, name: 'Foozle Cumberbunch'}]) }.should_not raise_error
-
-    record.collection.size.should == 1
-    record.collection.first.name.should == 'Foozle Cumberbunch'
+      record.single.name.should == 'wibble'
+    end
   end
 end
