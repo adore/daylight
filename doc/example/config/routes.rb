9c28ddedc98b980ac1f3bf2105b07f85bc4db291
Doug McInnes <doug@dougmcinnes.com>
2014-06-24 14:27:14 -0700

    example routes

--- doc/example/config/routes.rb
+++ doc/example/config/routes.rb
@@ -1,5 +1,17 @@
 DaylightExample::Application.routes.draw do
 
+  namespace :api, path: '' do
+    namespace :v1 do
+      with_options(except: [:new, :edit, :destroy]) do |r|
+        r.resources :companies, associated: [:blogs]
+        r.resources :blogs, associated: [:posts]
+        r.resources :users, associated: [:blogs, :posts, :comments]
+        r.resources :comments
+        r.resources :posts, associated: [:comments, :commenters, :suppressed_comments], remoted: [:top_comments]
+      end
+    end
+  end
+
   mount Daylight::Documentation => '/docs'
 
 end
