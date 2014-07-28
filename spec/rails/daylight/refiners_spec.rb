b54a4680d66375f6d916eefb21065783d9b61564
Doug McInnes <doug@dougmcinnes.com>
2014-07-28 12:03:07 -0700

    added test for registered_scopes uniqueness

--- spec/rails/daylight/refiners_spec.rb
+++ spec/rails/daylight/refiners_spec.rb
@@ -56,8 +56,19 @@ describe Daylight::Refiners::AttributeSeive do
 end
 
 describe Daylight::Refiners do
+  class AnotherRefinersTestClass < RefinerMockActiveRecordBase
+    scope :scope_c, -> { 'c' }
+  end
+
   it 'tracks registered scopes' do
     RefinersTestClass.registered_scopes.should == %w[scope_a scope_b]
+  end
+
+  it 'keeps track of different scopes on different classes' do
+    RefinersTestClass.registered_scopes.should == %w[scope_a scope_b]
+    AnotherRefinersTestClass.registered_scopes.should == %w[scope_c]
+
+    RefinerMockActiveRecordBase.scope :scope_x, -> { 'x' }
   end
 
   it 'returns true if scoped? finds a match' do
