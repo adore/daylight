0ae3a19d3766122b16d317b6921364ee46bda53e
Doug McInnes <doug@dougmcinnes.com>
2014-07-30 14:19:57 -0700

    update version to 0.9.0.rc2

--- lib/daylight/version.rb
+++ lib/daylight/version.rb
@@ -3,7 +3,7 @@ module Daylight
     MAJOR = 0
     MINOR = 9
     PATCH = 0
-    BUILD = 'rc1'
+    BUILD = 'rc2'
 
     Daylight::VERSION = STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
