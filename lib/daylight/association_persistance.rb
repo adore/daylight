e2f763e96154b58a8fe8cffd1df7dad3479b63c9
Doug McInnes <doug@dougmcinnes.com>
2014-08-19 16:02:41 -0700

    association persistance tests and changes
    changed? now returns true if the object is new
    make sure to recreate associations if they already exist in the
    attributes hash.

--- lib/daylight/association_persistance.rb
+++ lib/daylight/association_persistance.rb
@@ -14,7 +14,7 @@ module Daylight::AssociationPersistance
 
   # has our attributes changed since we were loaded?
   def changed?
-    attributes.hash != @attribute_hash_on_load
+    new? || attributes.hash != @attribute_hash_on_load
   end
 
   protected
@@ -23,22 +23,20 @@ module Daylight::AssociationPersistance
     def include_child_updates
       self.class.reflection_names.each do |reflection_name|
         association = instance_variable_get("@#{reflection_name}")
-        attribute_name = "#{reflection_name}_attributes"
+        reflection_attribute_name = "#{reflection_name}_attributes"
         # ignore associations that have not been set
         next unless association
-        # don't overwrite associations that already exist in the attributes hash
-        next unless attributes[attribute_name].nil?
 
         # recurse into the child(ren)
-        attributes[attribute_name] =
+        attributes[reflection_attribute_name] =
           if Enumerable === association
             # currently we need to send ALL the children if any of them
             # have changed
             association.each {|child| child.include_child_updates }
-            association.map(&:serializable_hash) if association.any?(&:changed?)
+            association.map(&:serializable_hash) if attributes[reflection_attribute_name].present? || association.any?(&:changed?)
           else
             association.include_child_updates
-            association.serializable_hash if association.changed?
+            association.serializable_hash if attributes[reflection_attribute_name].present? || association.changed?
           end
       end
     end
---
+++ spec/lib/daylight/association_persistance_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper'
+
+describe Daylight::AssociationPersistance do
+
+  class RelatedPersistanceTestClass < Daylight::API
+  end
+
+  class PersistanceTestClass < Daylight::API
+    has_many :children, class_name: 'RelatedPersistanceTestClass'
+    belongs_to :parent, class_name: 'RelatedPersistanceTestClass'
+
+    def parent_id ; 1 ; end
+  end
+
+  before do
+    data = {id: 1, name: 'test'}
+    stub_request(:get, %r{#{PersistanceTestClass.element_path(1)}}).to_return(body: data.to_json)
+    stub_request(:get, %r{#{RelatedPersistanceTestClass.element_path(1)}}).to_return(body: data.to_json)
+    stub_request(:get, %r{/persistance_test_classes/1/children\.json}).to_return(body: [data, data].to_json)
+  end
+
+  let(:object) { PersistanceTestClass.find(1) }
+
+  describe :changed? do
+
+    it 'returns false if the object is not modified' do
+      object.changed?.should be_false
+    end
+
+    it 'returns true if the object has a field modified' do
+      object.name = 'this is a change'
+
+      object.changed?.should be_true
+    end
+
+    it 'returns true if the object has a modified association' do
+      object.parent = RelatedPersistanceTestClass.new
+
+      object.changed?.should be_true
+    end
+
+    it 'returns true if the object is new' do
+      PersistanceTestClass.new.changed?.should be_true
+    end
+  end
+
+  describe :include_child_updates do
+    it 'does nothing if an association has not been loaded' do
+      object.send(:include_child_updates)
+      object.attributes['parent_attributes'].should be_nil
+      object.attributes['children_attributes'].should be_nil
+    end
+
+    it 'includes the single belongs_to/has_one assocation if it has changed' do
+      object.parent.should_not be_nil
+      object.parent.name = 'updated name'
+      object.send(:include_child_updates)
+      object.attributes['parent_attributes'].should be_present
+    end
+
+    it 'includes all children in a has_many type relation if any of them have changed' do
+      object.children.should be_present
+      object.children[0].name = 'updated name'
+
+      object.send(:include_child_updates)
+      object.attributes['children_attributes'].count.should == 2
+    end
+
+    it 'sets the association attribute to nil if nothing has changed in the collection' do
+      object.children.should be_present
+
+      object.send(:include_child_updates)
+      object.attributes['children_attributes'].should be_nil
+    end
+
+    it 'does not include the single child if it has not changed' do
+      object.parent.should be_present
+
+      object.send(:include_child_updates)
+      object.attributes['parent_attributes'].should be_nil
+    end
+
+    it 'still includes associations when there is a new child object' do
+      object.children.should be_present
+      object.children << RelatedPersistanceTestClass.new
+      object.children.count.should == 3
+
+      object.send(:include_child_updates)
+      object.attributes['children_attributes'].count.should == 3
+    end
+
+    it 'still includes associations when an additional persisted child object is added' do
+      object.children.should be_present
+      object.children << RelatedPersistanceTestClass.find(1)
+      object.children.count.should == 3
+
+      object.send(:include_child_updates)
+      object.attributes['children_attributes'].count.should == 3
+    end
+
+    it 'still includes has_one associations when persisted child object is set' do
+      object.parent.should be_present
+      object.parent = RelatedPersistanceTestClass.find(1)
+
+      object.send(:include_child_updates)
+      object.attributes['parent_attributes'].should be_present
+    end
+  end
+
+end
