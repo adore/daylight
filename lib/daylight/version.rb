763ff5351a2f1060e17677055461406a4030e0cb
Doug McInnes <doug@dougmcinnes.com>
2014-09-08 12:34:26 -0700

    1st Beta version 0.9.0!

--- lib/daylight/version.rb
+++ lib/daylight/version.rb
@@ -3,7 +3,7 @@ module Daylight
     MAJOR = 0
     MINOR = 9
     PATCH = 0
-    BUILD = 'rc3'
+    BUILD = nil
 
     Daylight::VERSION = STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
