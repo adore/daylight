a0bb6e23186b183b4141287a20f40bc1172b8e39
Reid MacDonald <reidmix@gmail.com>
2014-09-16 18:11:07 -0700

    using ActionDispatch::Static as our insertion point, Rack::Lock is gone

--- rails/extensions/request_id.rb
+++ rails/extensions/request_id.rb
@@ -1,6 +1,6 @@
 # Require server to support X-Request-Id
-if defined?(ActionDispatch::RequestId)
+if defined?(ActionDispatch::Static) && defined?(ActionDispatch::RequestId)
   ActiveSupport.on_load :before_initialize do
-    config.middleware.insert_before(::Rack::Lock, ActionDispatch::RequestId)
+    config.middleware.insert_after(ActionDispatch::Static, ActionDispatch::RequestId)
   end
 end
